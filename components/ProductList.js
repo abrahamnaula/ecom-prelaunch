22203af7b45dda795d74c3e4cfff92a0bff47ddf
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-06-09 13:51:11 -0400

    added product list and coming soon along with utils (first commit)

--- components/ProductList.js
+++ components/ProductList.js
@@ -7,7 +7,9 @@ const ProductList = ({products}) => {
                 <h2 className="text-2xl font-extrabold text-gray-900 mb-6">
                     Products
                 </h2>
-                <div className="grid grid-cols-1 gap-y-10 gap-x-6">
+                <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4
+                                xl:gap-x-8">
+
                     {
                         products.map(product => (
                             <ProductCard key={product.node.id} product={product}/>
---
+++ pages/comingSoon.jsx
@@ -0,0 +1,4 @@
+<form onSubmit={handleSubmit}>
+    <input type="email" onChange={handleChange} value={email} required />
+    <button type="submit">Submit</button>
+</form>
