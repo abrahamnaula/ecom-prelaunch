e0f70fb72b4192c96c65221e94057184e966f329
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-06-09 14:11:42 -0400

    comment for amplify

--- pages/comingSoon.jsx
+++ pages/comingSoon.jsx
@@ -1,5 +1,6 @@
-<form onSubmit={handleSubmit}>
+/*<form onSubmit={handleSubmit}>
     <input type="email" onChange={handleChange} value={email} required />
     <button type="submit">Submit</button>
 </form>
 
+*/
