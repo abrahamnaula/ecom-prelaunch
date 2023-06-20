8ffc0e30cbe2439fb92342dd5bd4de9ddc3dcf0f
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-06-20 16:38:46 -0400

    added gray era logo with fade

--- components/GrowingText/index.jsx
+++ components/GrowingText/index.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
 function GrowingText() {
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setIsLoaded(true), 1000); // start transition after 1 second
+        return () => clearTimeout(timer); // cleanup timer in case of unmounting
+    }, []);
+
     return (
-        <div className="flex items-center justify-center h-screen">
-            <h1 className="font-nhg text-bold text-gray-300 text-xs text-center grow">GRAY ERA</h1>
+        <div className={`flex items-center justify-center h-screen transition-opacity duration-1000 ease-in-out ${isLoaded ? 'opacity-100' : 'opacity-0'}`}>
+            <Image src="/new-ge-logo.png" alt="gray era logo" height={66} width={415}/>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      transitionProperty: {
+        'opacity': 'opacity',
+      },
     },
   },
   plugins: [
