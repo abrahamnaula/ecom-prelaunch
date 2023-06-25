9eb368d5a1526aa317b1bc1c0855da4eae7d5195
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-06-24 20:51:46 -0400

    removed usage of Nav and Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 import '../styles/globals.css'
-import BackgroundVideo from "../components/BackgroundVideo";
-import Logo from "../components/Logo";
 import React from "react";
-import Layout from "../components/Layout";
-
-
 export default function App({ Component, pageProps }) {
     return (
-        <Layout>
             <Component {...pageProps} />
-        </Layout>
     );
 }
 
