6f6955f8c307a13889cdd88b5d85a5938990f723
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-06-27 22:54:28 -0400

    checkpoint 1 on mobile/desktop collections menu

--- components/CollectionsMenu/CollectionMenuCard.js
+++ components/CollectionsMenu/CollectionMenuCard.js
@@ -2,7 +2,7 @@
 
 export default function CollectionMenuCard({ title, image, animationClass }) {
     return (
-        <div className={`w-full h-screen relative overflow-hidden mb-5 ml-1 mr-1 ${animationClass}`}>
+        <div className={`w-full sm:w-1/5 sm:h-screen h-52 relative overflow-hidden mb-1 ml-1 mr-1 ${animationClass}`}>
             <img className="object-cover w-full h-full" src={image} alt={title} />
             <div className="absolute inset-0 bg-black opacity-25"></div>
             <div className="absolute inset-0" style={{ backgroundColor: 'rgba(51, 51, 51, 0.72)' }}></div>
