eaa4e4c55e6a5cf5258e33bb02648d4c0e55bb79
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-06-28 23:28:13 -0400

    reordered the collection menu cards

--- pages/collections-menu.js
+++ pages/collections-menu.js
@@ -1,12 +1,18 @@
-// CollectionsMenu.js
-
 import Header from "../components/Header";
 import ShopHeader from "../components/ShopHeader";
 import DefaultFooter from "../components/DefaultFooter";
 import CollectionMenuList from '../components/CollectionsMenu/CollectionMenuList';
 import { fetchCollections } from '../lib/shopify';
 
+function reorderCollections(collections) {
+    const order = ["SHOP ALL", "BY ERA", "COLLECTIONS", "CATEGORIES", "SOLD ARCHIVE"];
+
+    return order.map(o => collections.find(c => c.title === o));
+}
+
 export default function CollectionsMenu({ collections }) {
+    const orderedCollections = reorderCollections(collections);
+
     return (
         <>
             <div className="fixed top-0 w-full z-50">
@@ -16,7 +22,7 @@ export default function CollectionsMenu({ collections }) {
             <div className="h-29px"></div>
             <div className="h-29px"></div>
             <div className="h-29px"></div>
-            <CollectionMenuList collections={collections} />
+            <CollectionMenuList collections={orderedCollections} />
             <div className="h-29px bg-black sm:bg-black sm:h-0"></div>
             <div id="top-element" className="-z-50 sm:bg-black"></div>
             <DefaultFooter/>
