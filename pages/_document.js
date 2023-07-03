59451a4eaa8dd4764b49de1b7bea3bc9e39c1d66
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-03 18:45:47 -0400

    disabled auto zoom for inputs

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+        <body>
         <Main />
         <NextScript />
       </body>
