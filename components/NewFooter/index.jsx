5b92bd4fa46ea7bacbf0f190052a4a4793dba615
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-03 17:33:36 -0400

    Footer social text is now centered and becomes stack when on mobile

--- components/NewFooter/index.jsx
+++ components/NewFooter/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function NewFooter() {
     return (
-        <footer className={`fixed bottom-0 left-0 w-full h-32 bg-gray-600 bg-opacity-50 flex flex-col justify-between font-nhg transition-all duration-500 ease-in-out transform`}>
+        <footer className={`w-full h-32 bg-gray-600 bg-opacity-50 flex flex-col justify-between font-ngh transition-all duration-500 ease-in-out transform`}>
             <div className="bg-grayBkg flex">
                 <div className="w-1/3 flex flex-col font-normal pt-3.5 mb-0.5">
                     <p className="font-ngh text-base tracking-wide pl-2 text-xxs mb-0.5 leading-tight">ABOUT & CONTACT</p>
@@ -11,11 +11,10 @@ function NewFooter() {
                     <p className="font-ngh text-base tracking-wide pl-2 text-xxs mb-0.5 leading-tight">PRIVACY POLICY</p>
                     <p className="font-ngh text-base tracking-wide pl-2 text-xxs mb-0.5 leading-tight">TERMS OF SERVICE</p>
                 </div>
-                <div className="w-1/3 flex justify-center items-center py-0.5">
-                    <a href="https://www.instagram.com/gray_era/" target="_blank" rel="noopener noreferrer" className="font-ngh text-base text-xs">
+                <div className="w-1/3 flex flex-col sm:flex-row justify-end sm:justify-center items-center py-0.5">
+                    <a href="https://www.instagram.com/gray_era/" target="_blank" rel="noopener noreferrer" className="font-ngh text-base text-xs mb-2 sm:mb-0 sm:mr-2">
                         INSTAGRAM
                     </a>
-                    <span className="mx-2 text-gray-400"> </span>
                     <p className="font-ngh text-base text-xs">TIKTOK</p>
                 </div>
             </div>
