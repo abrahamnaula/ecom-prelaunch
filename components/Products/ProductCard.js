de375e60ebb684909982f9c969d5b98508b3a161
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-04 16:51:42 -0400

    added dynamic Product Detail page

--- components/Products/ProductCard.js
+++ components/Products/ProductCard.js
@@ -1,4 +1,5 @@
 // components/ProductCard.js
+import Link from 'next/link';
 import Image from 'next/image';
 
 export default function ProductCard({ product }) {
@@ -9,21 +10,23 @@ export default function ProductCard({ product }) {
     const size = variants.edges[0]?.node?.title;
 
     return (
-        <div className="flex flex-col bg-white p-1 h-full">
-            <div className="relative flex-grow">
-                <Image
-                    src={imageUrl}
-                    alt={altText}
-                    layout="responsive"
-                    width={1920}
-                    height={2120}
-                    className="object-contain"
-                />
+        <Link href={`/products/${handle}`} passHref>
+            <div className="flex flex-col bg-white p-1 h-full cursor-pointer">
+                <div className="relative flex-grow">
+                    <Image
+                        src={imageUrl}
+                        alt={altText}
+                        layout="responsive"
+                        width={1920}
+                        height={2120}
+                        className="object-contain"
+                    />
+                </div>
+                <div className="mt-1 flex-grow">
+                    <h2 className="font-nhg font-medium text-black text-xs">{title}</h2>
+                    <p className="font-nhg font-medium text-black text-xs">{price} / {size}</p>
+                </div>
             </div>
-            <div className="mt-1 flex-grow">
-                <h2 className="font-nhg font-medium text-black text-xs">{title}</h2>
-                <p className="font-nhg font-medium text-black text-xs">{price} / {size}</p>
-            </div>
-        </div>
+        </Link>
     );
 }
