f68b4d54f2c376a18435a96f315ac5339542c867
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-06 20:01:33 -0400

    FOOTER FIX

--- components/NewFooter/index.jsx
+++ components/NewFooter/index.jsx
@@ -4,7 +4,7 @@ function NewFooter() {
     return (
         <footer>
             <div className="bg-grayBkg flex">
-                <div className="w-1/3 flex flex-col font-normal py-2 mb-0.5 mt-0 text-supsm ">
+                <div className="w-1/3 flex flex-col font-normal py-2 mb-0.5 mt-0 text-supsm">
                     <p className="font-ngh text-base tracking-wide pl-2 text-supsm sm:text-xxs text-white mb-0.5 leading-tight">ABOUT & CONTACT</p>
                     <p className="font-ngh text-base tracking-wide pl-2 text-supsm sm:text-xxs text-white mb-0.5 leading-tight">SHIPPING & RETURNS</p>
                     <p className="font-ngh text-base tracking-wide pl-2 text-supsm sm:text-xxs text-white mb-0.5 leading-tight">SIZING</p>
@@ -13,15 +13,21 @@ function NewFooter() {
                 </div>
                 <div className="w-1/3 flex flex-col sm:flex-row justify-end sm:justify-center items-center py-0.5 text-supsm sm:text-sm">
                     <a href="https://www.instagram.com/gray_era/" target="_blank" rel="noopener noreferrer"
-                       className="font-ngh text-base text-white mb-2 sm:mb-0 sm:mr-2 sm:text-xxs text-supsm">
+                       className="font-ngh text-base text-white mb-2 sm:mb-0 sm:mr-2
+                       sm:text-xxs text-supsm sm:block hidden">
                         INSTAGRAM
                     </a>
-                    <p className="font-ngh text-base text-supsm text-white sm:text-xxs">TIKTOK</p>
+                    <p className="font-ngh text-base text-supsm text-white sm:text-xxs sm:block hidden">TIKTOK</p>
+                </div>
+                <div className="flex flex-col items-end flex-grow justify-center">
+                    <a href="https://www.instagram.com/gray_era/" target="_blank" rel="noopener noreferrer"
+                       className="pr-2 pb-2 text-white text-supsm sm:text-xxs sm:hidden block">INSTAGRAM</a>
+                    <p className="pr-2 text-white text-supsm sm:text-xxs sm:hidden block">TIKTOK</p>
                 </div>
             </div>
-            <div className="h-10 sm:h-24 bg-gradient-to-r from-metallicLeft to-metallicRight">
-
-            </div>
+            <div className="h-10 sm:h-24 bg-gradient-to-r from-metallicLeft to-metallicRight text-black
+                            font-nhg text-supsm sm:text-xxs p-1">
+                © GRAY ERA 2023</div>
         </footer>
     );
 }
