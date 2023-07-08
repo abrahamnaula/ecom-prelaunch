224f0240fcbba5d581da49be15c119f0e99d671f
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-08 01:35:02 -0400

    Scrolling with load

--- components/Products/ProductCard.js
+++ components/Products/ProductCard.js
@@ -1,37 +1,37 @@
 // components/ProductCard.js
+import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import {formatter} from "../../utils/helpers";
-// components/ProductCard.js
+import { formatter } from "../../utils/helpers";
 
-export default function ProductCard({ product }) {
+const ProductCard = React.forwardRef(({ product }, ref) => {
     const { title, handle, images, priceRange, variants } = product;
-    const imageUrl = images.edges[0]?.node?.originalSrc;
+    const imageUrl = images.edges[0]?.node?.url;
     const altText = images.edges[0]?.node?.altText || "Product Image";
     const price = priceRange.minVariantPrice.amount;
     const size = variants.edges[0]?.node?.title;
 
     return (
         <Link href={`/products/${handle}`} passHref>
-            <div className="flex flex-col bg-white h-full cursor-pointer border-r border-gray-800">
+            <div ref={ref} className="flex flex-col bg-white h-full cursor-pointer border-r border-gray-800">
                 <div className="relative flex-grow ">
                     <Image
                         src={imageUrl}
                         alt={altText}
                         layout="responsive"
-                        width={2250}
-                        height={3000}
+                        width={2}
+                        height={3}
                         className="object-contain border-y border-gray-800"
                     />
                 </div>
                 <div className="flex-grow p-1 pl-3 border-b-3/4 border-gray-800">
-                    <h2 className="font-nhg font-medium text-black text-xxs sm:text-xs
-                                   transform-text: uppercase">{title}</h2>
-                    <p className="font-nhg font-medium text-black text-xxs sm:text-xs
-                                   transform-text: uppercase">{formatter.format(price)} / {size}</p>
+                    <h2 className="font-nhg font-medium text-black text-xxs sm:text-xs transform-text: uppercase">{title}</h2>
+                    <p className="font-nhg font-medium text-black text-xxs sm:text-xs transform-text: uppercase">{formatter.format(price)} / {size}</p>
                 </div>
             </div>
         </Link>
     );
-}
+});
+ProductCard.displayName = 'ProductCard';
 
+export default ProductCard;
--- components/Products/ProductList.js
+++ components/Products/ProductList.js
@@ -1,9 +1,83 @@
-import { useEffect } from 'react';
-import ProductCard from './ProductCard';
-
 // components/ProductList.js
+import { useState, useEffect, useRef } from 'react';
+import ProductCard from './ProductCard';
+import {ParamShopifyData} from "../../lib/shopify";
+import { useRouter } from 'next/router';
 
-export default function ProductList({ products }) {
+export default function ProductList({ initialProducts, hasNextPage }) {
+    const [products, setProducts] = useState(initialProducts);
+    const [loading, setLoading] = useState(!initialProducts);
+    const [cursor, setCursor] = useState(null);
+    const [hasMore, setHasMore] = useState(hasNextPage);
+    const observer = useRef();
+    const lastProductElementRef = useRef();
+
+    const router = useRouter()
+    const { collectionName } = router.query
+
+    const loadMore = async () => {
+        setLoading(true);
+        const query = `
+            query ($title: String!, $cursor: String) {
+                collections(first: 1, query: $title) {
+                    edges {
+                        node {
+                            id
+                            title
+                            handle
+                            products(first: 12, after: $cursor) {
+                                pageInfo {
+                                    hasNextPage
+                                }
+                                edges {
+                                    cursor
+                                    node {
+                                        id
+                                        title
+                                        handle
+                                        images(first: 1) {
+                                            edges {
+                                                node {
+                                                    altText
+                                                    url
+                                                }
+                                            }
+                                        }
+                                        priceRange {
+                                            minVariantPrice {
+                                                amount
+                                            }
+                                        }
+                                        variants(first: 1) {
+                                            edges {
+                                                node {
+                                                    title
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `;
+        const { data } = await ParamShopifyData(query, { title: collectionName, cursor: cursor });
+        const newProducts = data.collections.edges[0].node.products.edges.map(edge => {
+            return {
+                ...edge.node,
+                imageUrl: edge.node.images.edges[0]?.node?.url
+            };
+        });
+        const newCursor = data.collections.edges[0].node.products.edges.slice(-1)[0]?.cursor;
+        const newHasMore = data.collections.edges[0].node.products.pageInfo.hasNextPage;
+        setProducts(prevProducts => [...prevProducts, ...newProducts]);
+        setCursor(newCursor);
+        setHasMore(newHasMore);
+        setLoading(false);
+    };
+
     useEffect(() => {
         // When the component mounts, try to restore the scroll position.
         const scrollPosition = sessionStorage.getItem("scrollPosition");
@@ -18,11 +92,27 @@ export default function ProductList({ products }) {
         }
     }, []);
 
+    useEffect(() => {
+        if (loading) return;
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            console.log("Intersection Observer", entries);
+
+            if (entries[0].isIntersecting && hasMore) {
+                console.log("Load more");
+
+                loadMore();
+            }
+        })
+        if (lastProductElementRef.current) observer.current.observe(lastProductElementRef.current);
+    }, [loading, hasMore]);
+
     return (
         <div className="grid grid-cols-2 sm:grid-cols-4 gap-0 ">
-            {products.map((product) => (
-                <ProductCard key={product.id} product={product} />
+            {products.map((product, index) => (
+                <ProductCard key={product.id} product={product} ref={index === products.length - 1 ? lastProductElementRef : null} />
             ))}
+            {loading && 'Loading more products...'}
         </div>
     );
 }
