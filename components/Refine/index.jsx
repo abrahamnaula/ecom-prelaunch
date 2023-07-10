3d36bb112eb0cc9abd5ab8a955c7567fca50bda8
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-10 14:43:28 -0400

    border for build commented

--- components/Refine/index.jsx
+++ components/Refine/index.jsx
@@ -80,19 +80,19 @@ const FilterMenu = () => {
 
         return (
             <div className="pl-2 text-black py-1">
-                <div className="pb-1.5">TOPS SIZE:</div>
+                <div className="pb-1.5 tracking-wide font-semibold">TOPS SIZE</div>
                 <div className="flex pb-5 flex-wrap md:flex-nowrap">
                     {topsSizes.map(size =>
                         <p className="pr-12" onClick={() => handleFilterClick(size, 'sizes')} key={size}>{size}</p>
                     )}
                 </div>
-                <div className="pb-1.5">BOTTOMS SIZE:</div>
+                <div className="pb-1.5 tracking-wide font-semibold">BOTTOMS SIZE</div>
                 <div className="flex pb-5 flex-wrap md:flex-nowrap">
                     {bottomsSizes.map(size =>
                         <p className="pr-8" onClick={() => handleFilterClick(size, 'sizes')} key={size}>{size}</p>
                     )}
                 </div>
-                <div className="pb-1.5">OUTERWEAR SIZE:</div>
+                <div className="pb-1.5 tracking-wide font-semibold">OUTERWEAR SIZE</div>
                 <div className="flex pb-5 flex-wrap md:flex-nowrap">
                     {outerwearSizes.map(size =>
                         <p className="pr-12" onClick={() => handleFilterClick(size, 'sizes')} key={size}>{size}</p>
@@ -117,13 +117,17 @@ const FilterMenu = () => {
     }
 //
     return (
-        <div className="filter-menu border border-grayBd p-5 w-full sm:w-2/3 font-nhg font-medium text-xxs sm:text-sm">
+        //<div className="filter-menu border border-grayBd p-5 w-full sm:w-2/3 font-nhg font-medium text-xxs sm:text-sm">
+        <div className="filter-menu p-5 w-full sm:w-2/3 font-nhg font-medium text-xxs sm:text-sm">
             <button onClick={handleCancel} className="text-black h-8 sm:hidden">CANCEL</button>
-            <div className="filter-menu-header border border-red-500 sm:mb-12 h-8 flex justify-between items-center">
+            {/*<div className="filter-menu-header border border-red-500 sm:mb-12 h-8 flex justify-between items-center">*/}
+            <div className="filter-menu-header sm:mb-12 h-8 flex justify-between items-center">
                 <button onClick={handleClear} className="text-black w-14 sm:w-20 border border-black h-8 flex-none">CLEAR</button>
 
                 {/* This div will be visible on screens larger than mobile */}
-                <div className="pl-2 filter-list border border-green-500 text-black flex flex-wrap justify-start items-center flex-grow hidden sm:flex">
+                {/*<div className="pl-2 filter-list border border-green-500 text-black flex flex-wrap justify-start items-center flex-grow hidden sm:flex">
+ */}
+                <div className="pl-2 filter-list text-black flex flex-wrap justify-start items-center flex-grow hidden sm:flex">
                     {/* Display selected filters */}
                     {selectedFilters.map(filter =>
                         <SelectedFilter filter={filter} handleRemove={handleRemoveFilter} key={filter} />
@@ -132,28 +136,30 @@ const FilterMenu = () => {
                 <button onClick={handleCancel} className="text-black h-8 hidden sm:block">CANCEL</button>
             </div>
             {/* This div will be visible on mobile */}
-            <div className="pl-2 filter-list border border-green-500 text-black flex flex-wrap justify-start items-center flex-grow sm:hidden">
+            {/*<div className="pl-2 filter-list border border-green-500 text-black flex flex-wrap justify-start items-center flex-grow sm:hidden">
+*/}
+            <div className="pl-2 filter-list text-black flex flex-wrap justify-start items-center flex-grow sm:hidden">
                 {/* Display selected filters */}
                 {selectedFilters.map(filter =>
                     <SelectedFilter filter={filter} handleRemove={handleRemoveFilter} key={filter} />
                 )}
             </div>
-
-            <div className="filter-menu-categories border border-blue-500 mb-2 ">
-                <button onClick={() => setSelectedFilter('categories')} className="text-black pr-6 sm:pr-20 text-decoration-line: underline">CATEGORIES</button>
-                <button onClick={() => setSelectedFilter('collections')} className="text-black pr-6 sm:pr-20 text-decoration-line: underline">COLLECTIONS</button>
-                <button onClick={() => setSelectedFilter('byEra')} className="text-black pr-6 sm:pr-20 text-decoration-line: underline">BY ERA</button>
-                <button onClick={() => setSelectedFilter('sizes')} className="text-black pr-6 sm:pr-20 text-decoration-line: underline">SIZES</button>
+            {/*<div className="filter-menu-categories border border-blue-500 mb-2 ">*/}
+            <div className="filter-menu-categories mb-2 ">
+                <button onClick={() => setSelectedFilter('categories')} className="font-semibold text-black pr-6 sm:pr-20 text-decoration-line: underline">CATEGORIES</button>
+                <button onClick={() => setSelectedFilter('collections')} className="font-semibold text-black pr-6 sm:pr-20 text-decoration-line: underline">COLLECTIONS</button>
+                <button onClick={() => setSelectedFilter('byEra')} className="font-semibold text-black pr-6 sm:pr-20 text-decoration-line: underline">BY ERA</button>
+                <button onClick={() => setSelectedFilter('sizes')} className="font-semibold text-black pr-6 sm:pr-20 text-decoration-line: underline">SIZES</button>
             </div>
-
-            <div className="filter-menu-group-filters border border-yellow-500 mb-2">
+            {/*<div className="filter-menu-group-filters border border-yellow-500 mb-2">*/}
+            <div className="filter-menu-group-filters mb-2">
                 {selectedFilter === 'categories' && <CategoriesFilter />}
                 {selectedFilter === 'collections' && <CollectionsFilter />}
                 {selectedFilter === 'byEra' && <ByEraFilter />}
                 {selectedFilter === 'sizes' && <SizesFilter />}
             </div>
-
-            <div className="filter-menu-footer border border-purple-500">
+            {/*<div className="filter-menu-footer border border-purple-500">*/}
+            <div className="filter-menu-footer">
                 <button onClick={handleApply} className="h-8 w-full sm:w-1/3 sm:px-20 bg-black text-white text-decoration: underline ">APPLY FILTERS</button>
             </div>
         </div>
