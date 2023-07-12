3a2a950827934bc11aa726b90b0851fe96c1fed8
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-12 15:28:37 -0400

    SortMenu edit

--- components/SortMenu/index.jsx
+++ components/SortMenu/index.jsx
@@ -21,8 +21,9 @@ export default function SortMenu() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                 >
-                    <Menu.Items className="bg-bebe absolute right-0 w-56 origin-top-right divide-y divide-gray-100
-                                           shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="bg-bebe absolute mt-2 right-0 w-56 origin-top-right divide-y divide-gray-100
+                                           shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none
+                                           border border-black">
                         <div className="px-1 py-1">
                             <Menu.Item>
                                 {({ active }) => (
