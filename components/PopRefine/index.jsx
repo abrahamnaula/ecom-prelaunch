8a0e88835ddb2afa5535f0853cede146cc5ae05e
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-12 16:05:38 -0400

    Small Screen Left negative margin for Refine Pop

--- components/PopRefine/index.jsx
+++ components/PopRefine/index.jsx
@@ -23,7 +23,7 @@ export default function PopRefine() {
                             leaveFrom="opacity-100 translate-y-0"
                             leaveTo="opacity-0 translate-y-1"
                         >
-                            <Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2
+                            <Popover.Panel className="absolute left-1/2 -ml-2 sm:ml-0 z-10 mt-3 w-screen max-w-sm -translate-x-1/2
                                                       transform px-4 sm:px-0 lg:max-w-3xl">
                                 <div className="overflow-hidden shadow-lg ring-1 ring-black ring-opacity-5
                                                       border border-grayBkg">
