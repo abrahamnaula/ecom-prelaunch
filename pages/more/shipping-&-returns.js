23a7eb35fd45928818bfab2674fddf8f130a4a3f
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-13 16:20:19 -0400

    contents comment for space

--- pages/more/shipping-&-returns.js
+++ pages/more/shipping-&-returns.js
@@ -18,33 +18,10 @@ export default function ShippingReturns() {
                         </button>
                     </div>
 
-                    <div className="w-2/3 flex-1">
-                        <h1 className="mb-4 underline font-bold tracking-wide">ABOUT & CONTACT:</h1>
-                        <p className="mb-4">
-                            Gray Era is an online vintage store that currently operates out of Charlotte, NC.
-                        </p>
-                        <p className="mb-4">
-                            Each decade over the past 100 years provided us with a unique interpretation on the meaning and purpose of the clothes we wear. At Gray Era, we emphasize sourcing vintage garments that allow our customers to visualize what we consider the best of these distinct interpretations and decide what most resonates with them. Every single product we provide is deliberately handpicked by our sole owner, Michael Gray.
-                        </p>
-                        <h2 className="mb-4 tracking-wide font-bold">
-                            CONTACT US:
-                        </h2>
-                        <p>
-                            Direct Messaging
-                        </p>
-                        <p className="mb-4">
-                            <a className="font-semibold text-decoration-line: underline" href="https://www.instagram.com/gray_era/" target="_blank" rel="noopener noreferrer">@gray_era</a>
-                        </p>
-                        <p>
-                            Customer Service
-                        </p >
-                        <a  className="font-semibold text-decoration-line: underline" href="mailto:contact@grayeravintage.com">contact@grayeravintage.com</a>
-                        <p className="mt-4">
-                            Other Inquiries
-                        </p>
-                        <a className="font-semibold text-decoration-line: underline" href="mailto:Info@grayeravintage.com">
-                            Info@grayeravintage.com
-                        </a>
+                    <div className="w-2/3 flex-1 pb-8">
+                        {/*CONTENTS*/}
+
+
 
                     </div>
                 </div>
