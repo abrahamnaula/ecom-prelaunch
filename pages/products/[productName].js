0835f95337e26861157420606cff881730abcb64
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-13 17:27:44 -0400

    product page space for header

--- pages/products/[productName].js
+++ pages/products/[productName].js
@@ -74,7 +74,7 @@ export default function Product({ product }) {
             <div className="fixed w-full top-0 z-20">
                 <WorkHeader />
             </div>
-            <div className="h-8.5"></div>
+            <div className="h-14"></div>
 
             <main className="flex-grow flex flex-col lg:flex-row z-10">
                 <div className="flex flex-grow 3xl:h-full lg:w-24">
