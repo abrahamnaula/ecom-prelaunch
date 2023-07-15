d2c7771e491b5ba80c05601f91614a1876f89cd7
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-15 14:07:26 -0400

    Display of tags correct with incorrect use of history

--- components/FilterContext/index.jsx
+++ components/FilterContext/index.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState, useContext, useEffect} from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 
 const FilterContext = createContext();
 
@@ -9,6 +9,58 @@ export const FilterProvider = ({ children }) => {
     const [selectedSizes, setSelectedSizes] = useState([]);
     const [selectedFilter, setSelectedFilter] = useState(null);
     const [collectionName, setCollectionName] = useState(null);
+    const [filterHistory, setFilterHistory] = useState([]);
+
+    const categories = new Map([
+        ["shirts", "SHIRTS"],
+        ["tees", "TEES"],
+        ["bottoms", "BOTTOMS"],
+        ["outerwear", "OUTERWEAR"],
+        ["sweatshirts", "SWEATSHIRTS"],
+        ["headwear", "HEADWEAR"],
+        ["everything-else", "EVERYTHING ELSE"]
+    ]);
+
+    const collections = new Map([
+        ["music-art-film", "MUSIC, ART, & FILM"],
+        ["denim-workwear-military", "DENIM, WORKWEAR, & MILITARY"],
+        ["sportswear-streetwear", "SPORTSWEAR & STREETWEAR"],
+        ["blanks-essentials", "BLANKS & ESSENTIALS"],
+        ["women", "WOMEN"]
+    ]);
+
+    const eras = new Map([
+        ["y2k", "Y2K"],
+        ["1990s", "1990s"],
+        ["1980s", "1980s"],
+        ["1970s", "1970s"],
+        ["pre-1970s", "PRE 1970s"]
+    ]);
+
+    const topsSizes = new Map([
+        ["X-Small", "X-SMALL"],
+        ["Small", "SMALL"],
+        ["Medium", "MEDIUM"],
+        ["Large", "LARGE"],
+        ["X-Large", "X-LARGE"],
+        ["XX-Large", "XX-LARGE"],
+        ["XXX-Large", "XXX-LARGE"]
+    ]);
+
+    const bottomsSizes = new Map(
+        Array.from({ length: 16 }, (_, i) => [String(i + 25), String(i + 25)])
+    );
+
+    const outerwearSizes = new Map([
+        ["X-Small", "X-SMALL"],
+        ["Small", "SMALL"],
+        ["Medium", "MEDIUM"],
+        ["Large", "LARGE"],
+        ["X-Large", "X-LARGE"],
+        ["XX-Large", "XX-LARGE"],
+        ["XXX-Large", "XXX-LARGE"]
+    ]);
+
 
     useEffect(() => {
         const filters = [];
@@ -22,28 +74,28 @@ export const FilterProvider = ({ children }) => {
             filters.push(selectedEra);
         }
         filters.push(...selectedSizes);
-
+        setFilterHistory(prevHistory => [...prevHistory, filters]);
     }, [selectedCategory, selectedCollection, selectedEra, selectedSizes]);
 
     const handleFilterClick = (filter, category) => {
         switch (category) {
-            case 'categories':
+            case "categories":
                 setSelectedCategory(filter);
-                setSelectedFilter('categories');
+                setSelectedFilter("categories");
                 break;
-            case 'collections':
+            case "collections":
                 setSelectedCollection(filter);
-                setSelectedFilter('collections');
+                setSelectedFilter("collections");
                 break;
-            case 'byEra':
+            case "byEra":
                 setSelectedEra(filter);
-                setSelectedFilter('byEra');
+                setSelectedFilter("byEra");
                 break;
-            case 'sizes':
+            case "sizes":
                 if (!selectedSizes.includes(filter)) {
                     setSelectedSizes(prevFilters => [...prevFilters, filter]);
                 }
-                setSelectedFilter('sizes');
+                setSelectedFilter("sizes");
                 break;
             default:
                 break;
@@ -52,31 +104,50 @@ export const FilterProvider = ({ children }) => {
 
     const handleRemoveFilter = (filter, category) => {
         switch (category) {
-            case 'categories':
+            case "categories":
                 if (selectedCategory === filter) setSelectedCategory(null);
                 break;
-            case 'collections':
+            case "collections":
                 if (selectedCollection === filter) setSelectedCollection(null);
                 break;
-            case 'byEra':
+            case "byEra":
                 if (selectedEra === filter) setSelectedEra(null);
                 break;
-            case 'sizes':
+            case "sizes":
                 setSelectedSizes(prevFilters => prevFilters.filter(f => f !== filter));
                 break;
             default:
                 break;
         }
-    }
+    };
+
+    console.log("Filter History:", filterHistory);
 
     return (
-        <FilterContext.Provider value={{
-            selectedCategory, setSelectedCategory,
-            selectedCollection, setSelectedCollection,
-            selectedEra, setSelectedEra,
-            selectedSizes, setSelectedSizes,
-            collectionName, setCollectionName,
-            selectedFilter, handleFilterClick, handleRemoveFilter, setSelectedFilter }}>
+        <FilterContext.Provider
+            value={{
+                selectedCategory,
+                setSelectedCategory,
+                selectedCollection,
+                setSelectedCollection,
+                selectedEra,
+                setSelectedEra,
+                selectedSizes,
+                setSelectedSizes,
+                collectionName,
+                setCollectionName,
+                selectedFilter,
+                handleFilterClick,
+                handleRemoveFilter,
+                setSelectedFilter,
+                categories, // Provide the categories Map
+                collections, // Provide the collections Map
+                eras, // Provide the eras Map
+                topsSizes, // Provide the topsSizes Map
+                bottomsSizes, // Provide the bottomsSizes Map
+                outerwearSizes // Provide the outerwearSizes Map
+            }}
+        >
             {children}
         </FilterContext.Provider>
     );
