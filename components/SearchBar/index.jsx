182918d03ea67b41c5f10c93b80726a8f8baee3b
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-19 21:02:28 -0400

    SearchBar jsx edit

--- components/SearchBar/index.jsx
+++ components/SearchBar/index.jsx
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
 import { FaSearch } from "react-icons/fa";
-
-function SearchBar({ onSearch }) {
+import { useRouter } from 'next/router';
+function SearchBar() {
     const [inputValue, setInputValue] = useState('');
-
+    const router = useRouter();
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     }
-
     const handleKeyPress = (e) => {
         if (e.key === 'Enter') {
-            onSearch(inputValue);
+            handleSearch(e);
         }
     }
+    const handleSearch = (e) => {
+        e.preventDefault();
 
+        // Check if input is empty
+        if (!inputValue) return;
+
+        // Update the URL with the new search term
+        router.push(`/search/${inputValue}`);
+    };
     return (
         <div className="flex items-center">
-            <FaSearch className="mr-2 fill-black h-2 sm:h-3"/>
+            <button onClick={handleSearch}>
+                <FaSearch className="mr-2 fill-black h-2 sm:h-3"/>
+            </button>
             <input
-                className="uppercase placeholder-grayBkg font-nhg font-medium text-grayBkg text-xxs
+                className="placeholder-grayBkg font-nhg font-medium text-grayBkg text-xxs
                             sm:text-xxs sm:font-nhg sm:text-grayBkg sm:font-medium bg-transparent outline-none
-                            w-12 sm:w-auto" // Add this line
+                            w-12 sm:w-auto"
                 placeholder="SEARCH"
                 value={inputValue}
                 onChange={handleInputChange}
@@ -29,5 +38,4 @@ function SearchBar({ onSearch }) {
         </div>
     )
 }
-
 export default SearchBar;
