b7d3944c16b2223613e9896dde95674e9a36c6ee
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-25 13:15:14 -0400

    fetching 60 per page

--- pages/testing/[id].js
+++ pages/testing/[id].js
@@ -81,7 +81,7 @@ function Pagination({ currentPage, totalPages, setCurrentPage }) {
 }
 
 
-const productsPerPage = 20
+const productsPerPage = 60
 export default function Collection({ initialProducts, hasNextPage, totalProductCount }) {
     const router = useRouter();
     const { formattedFilters } = useFilter();
@@ -238,7 +238,7 @@ export default function Collection({ initialProducts, hasNextPage, totalProductC
 }
 export async function getServerSideProps(context) {
     const { id, page } = context.query;
-    const productsPerPage = 20;
+    const productsPerPage = 60;
     const pageNumber = parseInt(page) || 1;  // set page number to 1 if it's not defined
 
     const query = `
