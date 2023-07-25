44a14e032db4064e6fdb12b09bb34f36a913f261
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-24 23:34:23 -0400

    next.config with env's

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   assetPrefix: '/',
   env: {
     SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
-    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN,
+    SHOPIFY_ADMIN_ACCESSTOKEN: process.env.SHOPIFY_ADMIN_ACCESSTOKEN,
   },
   images: {
     domains: ['cdn.shopify.com']
