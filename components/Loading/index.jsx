0db674ebc7ea14fdd6ec26617a964f9284dbf4c0
Abraham Naula <62670655+abrahamnaula@users.noreply.github.com>
2023-07-27 16:52:47 -0400

    loading  finished

--- components/Loading/index.jsx
+++ components/Loading/index.jsx
@@ -3,13 +3,8 @@ import Image from 'next/image';
 
 export default function Loading() {
     return (
-        <div className="h-screen w-screen bg-black flex items-center justify-center">
-            <div className="flex flex-col justify-center items-center h-full">
-                <div className="animate-reveal flex flex-col items-center justify-center">
-                    <Image src="/new-ge-logo.png" alt="gray era logo" height={199} width={199} />
-                    <p className="font-nhg font-medium text-white mt-4 tracking-widest">LOADING...</p>
-                </div>
-            </div>
+        <div className="h-screen w-screen bg-black ">
+                <p className="font-nhg font-medium text-white tracking-widest p-1">LOADING...</p>
         </div>
     );
 }
